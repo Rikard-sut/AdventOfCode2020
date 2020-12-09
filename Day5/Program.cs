fde3090e7a3ffc2103fc14a19f4aad5e6fdf1eb8
rikard.skoglund <rikard.skoglund@starrepublic.com>
2020-12-09 12:07:18 +0100

    day 5 pt 1 done

---
+++ Day5/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using Extensions;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\admin\source\repos\AdventOfCode2020\Day5\Data5.txt";
+            var data = filepath.GetData();
+            var seatIds = FindSeatIds(data);
+            int highestId = FindHighestId(seatIds);
+            Console.WriteLine(highestId);
+            Console.ReadKey();
+        }
+
+        public static List<int> FindSeatIds(List<object> seatcodes)
+        {
+            List<int> seatIds = new List<int>();
+            int[] rows;
+            int[] seats;
+
+            foreach (string seatCode in seatcodes)
+            {
+                rows = GetRows();
+                seats = new int[] { 0, 1, 2, 3, 4, 5, 6, 7 };
+                foreach (char letter in seatCode)
+                {
+
+                    int possibleMaxRow;
+                    int possibleMinRow;
+
+                    if (letter == 'F' || letter == 'f')
+                    {
+                        possibleMinRow = rows[0];
+                        possibleMaxRow = rows[rows.Length / 2 - 1];
+                        rows = CopyValues(possibleMinRow, possibleMaxRow, rows);
+                    }
+
+                    if (letter == 'B' || letter == 'b')
+                    {
+                        possibleMinRow = rows[rows.Length / 2];
+                        possibleMaxRow = rows[rows.Length - 1];
+                        rows = CopyValues(possibleMinRow, possibleMaxRow, rows);
+                    }
+
+                    int possibleMaxSeat;
+                    int possibleMinSeat;
+
+                    if (letter == 'L' || letter == 'l')
+                    {
+                        possibleMinSeat = seats[0];
+                        possibleMaxSeat = seats[seats.Length / 2 -1];
+                        seats = CopyValues(possibleMinSeat, possibleMaxSeat, seats);
+                    }
+
+                    if (letter == 'R' || letter == 'r')
+                    {
+                        possibleMinSeat = seats[seats.Length / 2];
+                        possibleMaxSeat = seats[seats.Length - 1];
+                        seats = CopyValues(possibleMinSeat, possibleMaxSeat, seats);
+                    }
+                }
+
+                int seatid = rows[0] * 8 + seats[0];
+                seatIds.Add(seatid);
+            }
+            return seatIds;
+        }
+
+        public static int[] GetRows()
+        {
+            int[] rows = new int[128];
+            for (int i = 0; i < rows.Length; i++)
+            {
+                rows[i] = i;
+            }
+            return rows;
+        }
+
+        public static int[] CopyValues(int min, int max, int[] arrayToCopy)
+        {
+            var list = new List<int>();
+            var newArrary = new int[arrayToCopy.Length / 2];
+
+            for (int i = 0; i < arrayToCopy.Length; i++)
+            {
+                if (arrayToCopy[i] >= min && arrayToCopy[i] <= max)
+                {
+                    list.Add(arrayToCopy[i]);
+                }
+            }
+
+            return list.ToArray();
+        }
+
+        public static int FindHighestId(List<int> seatIds)
+        {
+            int highestid = 0;
+            int currentId;
+            foreach (int id in seatIds)
+            {
+                currentId = id;
+                if (highestid < currentId)
+                    highestid = currentId;
+            }
+            return highestid;
+        }
+    }
+}
